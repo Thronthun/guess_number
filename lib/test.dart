2a5c3293d1b064da7daf66c0698af26658e6e2ca
Thronthun Ruansirilert <mokykanga23@gmail.com>
2021-12-12 23:12:20 +0700

    Initial commit

--- lib/test.dart
+++ lib/test.dart
@@ -1,17 +1,30 @@
 import 'dart:io';
 import 'dart:math';
-
+//ignore_for_file: avoid_print
 void main (){
   var r = Random();
   var answer = r.nextInt(10);
+  var num = 0;
   for(;;) {
-    stdout.write("Please enter your input: ");
+    stdout.write("Guess the number between 1 and 100: ");
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!);
-    if(guess == answer) {
-      stdout.write("AWESOME!!!");
-      break;
+    if(guess != null) {
+      num++;
+      if (guess == answer) {
+        stdout.write(guess);
+        stdout.write(" ♥AWESOME♥ , total guesses: ");
+        stdout.write(num);
+        break;
+      }
+      if (guess > answer) {
+        stdout.write(guess);
+        print(" IS TOO HIGH!▲");
+      }
+      if (guess < answer) {
+        stdout.write(guess);
+        print(" IS TOO LOW!▼");
+      }
     }
-    print("WRONG!!!");
   }
 }
